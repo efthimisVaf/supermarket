1cdef7a7a28e91cecee3eada13a45febf1f19d2a
Efthimis Vafeidis <efthimisvaf@pop-os.localdomain>
2020-07-18 01:56:16 +0200

    fixed update request

--- src/main/java/com/utrechtfour/supermarket/service/CategoryService.java
+++ src/main/java/com/utrechtfour/supermarket/service/CategoryService.java
@@ -3,11 +3,8 @@ package com.utrechtfour.supermarket.service;
 import com.utrechtfour.supermarket.model.Category;
 import com.utrechtfour.supermarket.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
-import java.security.PublicKey;
 import java.util.Optional;
 
 @Service
